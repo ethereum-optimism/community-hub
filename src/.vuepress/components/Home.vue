88a14dc89bfc2849ac7b4a55ab8035394ebfe2ca
smartcontracts <kelvinfichter@gmail.com>
2021-05-18 22:07:08 -0400

    feat: add testnet page (#110)

--- src/.vuepress/components/Home.vue
+++ src/.vuepress/components/Home.vue
@@ -21,6 +21,7 @@
       <a class="action-button" href="https://github.com/ethereum-optimism/optimism-tutorial/blob/main/README.md">try the <u>tutorial</u></a>
       <a class="action-button" href="/docs">read the <u>docs</u></a>
       <a class="action-button" href="https://discord.com/invite/jrnFEvq">come hang on <u>discord</u></a>
+      <a class="action-button" href="/testnet">deploy to <u>testnet</u></a>
     </header>
   </main>
 </template>
