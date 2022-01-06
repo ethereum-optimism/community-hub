5127a4bfda14ad99240104c31e15b2a574ff6761
Kelvin Fichter <kelvin@optimism.io>
2022-01-06 15:00:43 -0500

    fix: add cross domain fees page back in

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -73,7 +73,8 @@ module.exports = {
             // Common (standard bridge) before
             // rare (messaging)
             '/docs/developers/bridge/standard-bridge.md',
-            '/docs/developers/bridge/messaging.md'
+            '/docs/developers/bridge/messaging.md',
+            '/docs/developers/bridge/cross-domain-fees.md',
           ],
           collapsable: true,
         },
