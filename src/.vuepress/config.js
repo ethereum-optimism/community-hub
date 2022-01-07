b34d5f918e0b2102d698574be76b32ae59984412
Kelvin Fichter <kelvin@optimism.io>
2022-01-06 22:23:55 -0500

    fix: use regular fonts instead of solid

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
     darkmode: 'disable',
     themeColor: false,
     blog: false,
-    iconPrefix: 'fas fa-',
+    iconPrefix: 'far fa-',
     algolia: {
       apiKey: '47d21d4ea72ed7cb504b1c6c0a46b5a0',
       indexName: 'optimism'
