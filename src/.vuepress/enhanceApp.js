91540b46f6a2b6d332adc6290f4c73c576454440
tre <tremarkley@gmail.com>
2023-03-15 15:50:03 -0700

    revert doc test change

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -8,6 +8,10 @@ export default ({ router }) => {
   ])
 }
 
+// When new content is detected by the app, this will automatically
+// refresh the page, so that users do not need to manually click
+// the refresh button. For more details see:
+// https://linear.app/optimism/issue/FE-1003/investigate-archive-issue-on-docs
 const autoReload = () => {
     event.$on('sw-updated', e => e.skipWaiting().then(() => {
       location.reload(true);
