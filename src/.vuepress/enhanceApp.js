ef89965bce2d077dc1e8ad1723c8b6854e4cd5db
tre <tremarkley@gmail.com>
2023-03-15 15:09:24 -0700

    add auto-reload

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,5 +1,16 @@
+import event from '@vuepress/plugin-pwa/lib/event'
+
 export default ({ router }) => {
+  autoReload();
+  
   router.addRoutes([
     { path: '/docs/', redirect: '/' },
   ])
+}
+
+const autoReload = () => {
+    event.$on('sw-updated', e => e.skipWaiting().then(() => {
+      console.log('SW UPDATED AND RELOADING');
+      location.reload(true);
+    }))
 }
